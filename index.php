d4ef02809f9e1e366ea066f8cf69340e7a3394dc
Brenden Evans <bhevans17@gmail.com>
2019-02-07 06:12:26 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <title>ShowTrackr | Track Your Favorite Shows</title>
+</head>
+<body>
+
+<!-- Header -->
+    <header>
+
+<!-- Navigation Bar -->
+        <nav>
+            <a id="show-trackr-logo" href="#">Show<span>Trackr</span></a>
+            <button id="get-started-btn">Get started</button>
+       
+        </nav>
+
+<!-- Sign Up Section -->
+        <section id="sign-up-section">
+            <h1>Track your shows automatically</h1>
+            <p>With ShowTrackr you can track your favorite TV shows automatically, so you never loose track of your TV shows ever again. 🍿</p>
+            
+            <!-- Sign Up Section Form -->
+            <form id="sign-up-form">
+            </form>
+        </section>
+    </header>
+</body>
+</html>
